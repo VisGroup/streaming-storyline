95356e4dc88adda67b64ba064dc214a1c0c3242b
Zhou Bowei <zhoubw12@mails.tsinghua.edu.cn>
2016-11-20 18:48:10 +0800

    :see_no_evil: 服务器添加ejs，测试长连接

---
+++ server/public/js/tools.js
@@ -0,0 +1,10 @@
+var hashcode = function() {
+    var hash = 0;
+    if (this.length == 0) return hash;
+    for (var i = 0; i < this.length; i++) {
+        var character = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + character;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+};
