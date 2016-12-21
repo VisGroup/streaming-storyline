5b0ce4c219ff01418c215d98808e66b9d68acd84
Travis-Xiao <travis.xiao@gmail.com>
2016-12-22 00:25:07 +0800

    compaction refined

--- optimizer/layout_optimizer.py
+++ optimizer/layout_optimizer.py
@@ -1,9 +1,11 @@
 import mosek
 from numpy import array, zeros, ones
+import operator
 
-inf = 0.0
 
+INF = 0.
 env = mosek.Env()
+
 
 def optimize(Q, c, A, b, numvar, numcon, objsense):
 	task = env.Task()
@@ -14,7 +16,7 @@ def optimize(Q, c, A, b, numvar, numcon, objsense):
 	for j in range(numvar):
 		# Set the bounds on variable j
 		# blx[j] <= x_j <= bux[j]
-		task.putbound(mosek.accmode.var, j, mosek.boundkey.lo, 0.0, inf)
+		task.putbound(mosek.accmode.var, j, mosek.boundkey.lo, 0.0, INF)
 
 	asub, aval = A
 	for i in range(numcon):
@@ -51,29 +53,59 @@ def optimize(Q, c, A, b, numvar, numcon, objsense):
 		return solsta
 
 
-INF = 0.
-
 def compaction(current, preslice, config):
+	has_precendent = "time" in preslice
 	current_order, current_session = convert_format(current)
-	preslice_order, preslice_session = convert_format(preslice)
+	if has_precendent:
+		preslice_order, preslice_session = convert_format(preslice)
+
+		HEIGHT_MIN = 10
+		# get preslice height map
+		preslice_height_map = {}
+		for session in preslice["sessions"]:
+			for entity in session:
+				preslice_height_map[entity] = session[entity] + HEIGHT_MIN
 
 	numvar = len(current_order)
 	numcon = numvar - 1
 
-	# TODO: construction of Q
+	# construction of Q
 	qsubi = []
 	qsubj = []
 	qval = []
-	diagon_visited = []
+	c = []
 	for i in range(numvar):
-		diagon_visited.append(False)
-
-	for i, status in enumerate(diagon_visited):
-		if status:
-			continue
 		qsubi.append(i)
 		qsubj.append(i)
 		qval.append(1.)
+		c.append(0.)
+
+	if has_precendent:
+		for i in range(numvar):
+			for j in range(numvar):
+				if i == j:
+					continue
+				if i not in preslice_session or j not in preslice_session:
+					continue
+				if current_session[i] == current_session[j]:
+					continue
+				if preslice_session[i] == preslice_session[j]:
+					qval[i] += 1.
+					qval[j] += 1.
+					qsubi.append(i)
+					qsubj.append(j)
+					qval.append(-1.)
+				else:
+					ratio = preslice_height_map[i] / preslice_height_map[j]
+					if ratio < 1.:
+						qval[i] += 1 / ratio
+						qval[j] += 1.
+					else:
+						qval[i] += 1.
+						qval[j] += ratio
+					qsubi.append(i)
+					qsubj.append(j)
+					qval.append(ratio)
 
 	# construction of A
 	asub = []
@@ -97,7 +129,6 @@ def compaction(current, preslice, config):
 	res = optimize((qsubi, qsubj, qval), c, (asub, aval), (bkc, blc, buc), numvar, numcon, objsense)
 	print(res)
 
-import operator
 
 def convert_format(slice):
 	time = slice["time"]
@@ -114,16 +145,26 @@ def convert_format(slice):
 
 	for item in sorted(heights.items(), key=operator.itemgetter(1)):
 		order.append(item[0])
-
 	return (order, session_map)
 
 
-if __name__ == '__main__':
+def test(l_c, l_p):
 	import json
 	from config import CONFIG
-	c = '{"time":5,"sessions":[{"0": 0},{"1": 2,"2": 3},{"3": 5},{"4": 7},{"5": 9},{"6": 11,"7": 12},{"8": 14}]}'
-	p = '{  "sessions": [    {      "0": 0    },     {      "1": 2,       "2": 3    },     {      "3": 5    },     {      "4": 7    },     {      "5": 9    },     {      "6": 11,       "7": 12    },     {      "8": 14    }  ],   "time": 4}'
-	current = json.loads(c)
-	preslice = json.loads(p)
+	# c = '{"time":5,"sessions":[{"0": 0},{"1": 2,"2": 3},{"3": 5},{"4": 7},{"5": 9},{"6": 11,"7": 12},{"8": 14}]}'
+	# p = '{  "sessions": [    {      "0": 0    },     {      "1": 2,       "2": 3    },     {      "3": 5    },     {      "4": 7    },     {      "5": 9    },     {      "6": 11,       "7": 12    },     {      "8": 14    }  ],   "time": 4}'
+	current = json.loads(l_c)
+	preslice = json.loads(l_p)
 
 	compaction(current, preslice, CONFIG)
+
+
+if __name__ == '__main__':
+	with open("data.txt") as infile:
+		lines = []
+		for line in infile:
+			lines.append(line)
+
+		print(len(lines))
+		for i in range(int(len(lines) / 2)):
+			test(lines[i], lines[i + 1])
--- server/storyline-layout/build.sh
+++ server/storyline-layout/build.sh
@@ -1,4 +1,5 @@
 g++ -c StorylineSession.cpp
 g++ -c StorylineDataSlice.cpp
 g++ -c StorylineLayout.cpp
-g++ -o gintama storyline.cpp StorylineSession.o StorylineDataSlice.o StorylineLayout.o
+g++ -c tools.cpp
+g++ -o gintama main.cpp StorylineSession.o StorylineDataSlice.o StorylineLayout.o tools.o
